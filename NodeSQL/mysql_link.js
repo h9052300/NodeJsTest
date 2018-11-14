8e885afd6aa2b3cf6c6c8bbb99ab8151d15a886b
h9052300 <h9052300@gmail.com>
2018-11-14 16:05:37 +0800

    11/14練習

---
+++ NodeSQL/mysql_link.js
@@ -0,0 +1,22 @@
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'h3141200',
+    database: 'onepiece'
+});
+
+connection.connect();
+
+let sql = "SELECT * FROM users";
+connection.query(sql, function (error, results, fields) {
+    // console.log(error);
+    // console.log('---------------------------------------');
+    // console.log(results);
+
+    for (let i = 0; i < results.length; i++) {
+        console.log(results[i].name);
+    }
+});
+
+connection.end();
