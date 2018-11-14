34f3c48d3fe425833fcf7bdf6898d9f16deae80e
h9052300 <h9052300@gmail.com>
2018-11-14 17:39:14 +0800

    Update mysql_link.js

--- NodeSQL/mysql_link.js
+++ NodeSQL/mysql_link.js
@@ -2,7 +2,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'h3141200',
+    password: '',
     database: 'onepiece'
 });
 
